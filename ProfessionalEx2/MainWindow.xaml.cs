c737116ad168eb88dc80b0673021ab2ad298e77a
korolka <korolka212@gmail.com>
2023-07-20 16:25:29 +0300

    new commit

---
+++ ProfessionalEx2/MainWindow.xaml.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Timers;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Threading;
+
+namespace ProfessionalEx2
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        SqlConnection sqlConnection;
+        DispatcherTimer dispatcherTimer;
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Conect_Click(object sender, RoutedEventArgs e)
+        {
+            ConectToServerAsync();
+            Timer();
+            
+        }
+
+        private async void ConectToServerAsync()
+        {
+            Task task = new Task(ConectToServer);
+            Thread.Sleep(1000);
+            task.Start();
+            await task;
+        }
+
+        private void ConectToServer()
+        {
+            string conectionStringSql = @"Data Source=.;Initial Catalog=mobileappdb;Integrated Security=True";
+
+            using (sqlConnection = new SqlConnection(conectionStringSql))
+            {
+
+                string text1 = "«Підключено до бази даних»";
+                text.Dispatcher.Invoke(() => { text.Text = text1; });
+            }
+        }
+
+        private void DisconectToServer(SqlConnection sqlConnection)
+        {
+            using (sqlConnection)
+            {
+                try
+                {
+                    sqlConnection.Close();
+                    text.Text = "Disconect to database";
+                }
+                catch (Exception ex)
+                {
+                    text.Text= "Slq conection didn`t created. First create conection. Exception: "+ex.Message;
+                }
+            }
+        }
+        private void Timer_Tick(object sender, EventArgs e)
+        {
+            text.Dispatcher.Invoke(() => { text.Text += "\nДані отримані"; });
+        }
+
+        public void Timer()
+        {
+            dispatcherTimer = new DispatcherTimer();
+            dispatcherTimer.Tick += new EventHandler(Timer_Tick);
+            dispatcherTimer.Interval = new TimeSpan(0,0,3);
+            dispatcherTimer.Start();
+        }
+
+        private void Disconect_Click(object sender, RoutedEventArgs e)
+        {
+            DisconectToServer(sqlConnection);
+            dispatcherTimer.Stop();
+        }
+    }
+}
---
+++ ProfessionalLesson14/Program.cs
@@ -0,0 +1,38 @@
+﻿//Завдання 4
+
+//Переробіть додаткове завдання з уроку №11 із використанням конструкції async await.
+namespace ProfessionalLesson14
+{
+    internal class Program
+    {
+        static int counter = 0;
+
+        static object block = new object(); // block - не повинен бути структурним.
+
+        static void Function()
+        {
+            Console.WriteLine("Method function:" + Thread.CurrentThread.ManagedThreadId);
+            for (int i = 0; i < 50; ++i)
+            {
+                Console.WriteLine(++counter);
+                // Виконання деякої роботи потоком ...
+                Thread.Sleep(200);
+            }
+        }
+
+        public static async void FunctionAsync()
+        {
+            Task task = new Task(Function);
+            task.Start();
+            await task;
+        }
+
+        static void Main()
+        {
+            Console.WriteLine("Method main:" + Thread.CurrentThread.ManagedThreadId);
+            FunctionAsync();
+            // Delay
+            Console.ReadKey();
+        }
+    }
+}
